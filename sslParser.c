f6b1bffbdf4d00839ea35808a6468f4e0fbfbff8
Patrik Ondriga <xondri08@vutbr.cz>
2020-11-10 23:03:44 +0100

    Find out how to pars ssl header

--- sslParser.c
+++ sslParser.c
@@ -23,64 +23,85 @@
 #include <pcap/pcap.h>
 #endif
 
-#define SIZE_ETHERNET 14 //offset of Ethernet header to L3 protocol
-#define JUMP_SSL_HEADER_IPv4 54 //skip to the ssl header if comunication using IPv4
-#define JUMP_SSL_HEADER_IPv6 86 //skip to the ssl header if comunication using IPv6
+#define SIZE_IPv4_ETHERNET 14 //offset of IPv4 Ethernet header to L3 protocol
+#define SIZE_IPv6_ETHERNET 13 //offset of IPv6 Ethernet header to L3 protocol
 
-void tcp_handler(struct tcphdr *my_tcp, const u_char *packet){
-    printf("\tSrc port = %d, dst port = %d, seq = %u",ntohs(my_tcp->th_sport), ntohs(my_tcp->th_dport), ntohl(my_tcp->th_seq));
-
-    if (my_tcp->th_flags & TH_SYN)
-        printf(", SYN");
-    if (my_tcp->th_flags & TH_FIN)
-        printf(", FIN");
-    if (my_tcp->th_flags & TH_RST)
-        printf(", RST");
-    if (my_tcp->th_flags & TH_PUSH)
-        printf(", PUSH");
-    if (my_tcp->th_flags & TH_ACK)
-        printf(", ACK");
-    printf("\n");
+void tcp_handler(struct tcphdr *my_tcp, const u_char *packet, char* src_IP, char* dest_IP, const u_char* ssl_header){
+    if(my_tcp->th_flags & TH_SYN){
+        //TODO zaciatok komunikacie
+        int src_PORT = ntohs(my_tcp->th_sport);
+        int dest_PORT = ntohs(my_tcp->th_dport);
+    }
+    if((my_tcp->th_flags & TH_PUSH) && (my_tcp->th_flags & TH_ACK)){
+        //TODO kontrola, ci je ssl, ak je, potom spracuj
+        int length = htons(*(ssl_header+3)) + (*(ssl_header+4));
+    }
+    if(my_tcp->th_flags & TH_FIN){
+        //TODO ukoncit a vyhodnotit
+    }
 }
 
-void mypcap_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
+void mypcap_handler(const struct pcap_pkthdr header, const u_char *packet, ssl_con* ssl_list){
     struct ip *my_ip;               // pointer to the beginning of IP header
     struct ether_header *eptr;      // pointer to the beginning of Ethernet header
     u_int size_ip;
 
+    char* src_IP;
+    char* dest_IP;
+    const u_char* tcp_header;
+    struct tcphdr *my_tcp; // pointer to the TCP header
+
+    int size_TCP; //size of TCP header
+    const u_char* ssl_header; //skip to ssl header 
+
     eptr = (struct ether_header *) packet;
-    my_ip = (struct ip*) (packet+SIZE_ETHERNET);        // skip Ethernet header
-    size_ip = my_ip->ip_hl*4;                           // length of IP header
-    
+
     switch (ntohs(eptr->ether_type)){
         case ETHERTYPE_IP: // IPv4 packet
+            my_ip = (struct ip*) (packet+SIZE_IPv4_ETHERNET);        // skip Ethernet header
+            size_ip = my_ip->ip_hl*4;                           // length of IP header
+            src_IP = inet_ntoa(my_ip->ip_src);
+            dest_IP = inet_ntoa(my_ip->ip_dst);
 
-            //TODO  printf("\tEthernet type is  0x%x, i.e. IP packet \n", ntohs(eptr->ether_type));           
-            /*TODO
-            printf("\tIP: id 0x%x, hlen %d bytes, version %d, total length %d bytes, TTL %d\n",ntohs(my_ip->ip_id),size_ip,my_ip->ip_v,ntohs(my_ip->ip_len),my_ip->ip_ttl);
-            printf("\tIP src = %s, ",inet_ntoa(my_ip->ip_src));
-            printf("IP dst = %s",inet_ntoa(my_ip->ip_dst));
-            */
+            tcp_header = packet+SIZE_IPv4_ETHERNET+size_ip;
+            my_tcp = (struct tcphdr *) tcp_header;
 
             if(my_ip->ip_p == 6){
-                struct tcphdr *my_tcp = (struct tcphdr *) (packet+SIZE_ETHERNET+size_ip); // pointer to the TCP header
-                printf("####################################################=%d=%d\n", *(packet+SIZE_ETHERNET+size_ip+4), *(packet+SIZE_ETHERNET+size_ip+4+1)); //TODO debug
-                printf("##%d##\n", *(packet+54));
-                printf("##%d##\n", *(packet+54+3));
-                printf("##%d##\n", ((*(packet+54+3))+(*(packet+54+4))));
-                short length = htons(*(packet+54+3));
-                printf("length = %d\n\n", length);
-                tcp_handler(my_tcp, packet);
+                size_TCP = (*(tcp_header+12) & 0xf0) >> 2;
+                ssl_header = tcp_header + size_TCP;           
+                tcp_handler(my_tcp, packet, src_IP, dest_IP, ssl_header);
             }
             break;
         
         case ETHERTYPE_IPV6:  // IPv6 packet
-            //TODO  printf("\tEthernet type is 0x%x, i.e., IPv6 packet\n",ntohs(eptr->ether_type));
+            /*TODO treba dorobit
 
+            //TODO  printf("\tEthernet type is 0x%x, i.e., IPv6 packet\n",ntohs(eptr->ether_type));
+            my_ip = (struct ip*) (packet+SIZE_IPv6_ETHERNET);        // skip Ethernet header
+            size_ip = my_ip->ip_hl*4;                           // length of IP header
+            src_IP = inet_ntoa(my_ip->ip_src);
+            dest_IP = inet_ntoa(my_ip->ip_dst);
+
+            tcp_header = packet+SIZE_IPv6_ETHERNET+size_ip;
+            my_tcp = (struct tcphdr *) tcp_header;
+            printf("#######################%d#\n", my_ip->ip_p);//TODO debug
             if(my_ip->ip_p == 6){
-                struct tcphdr *my_tcp = (struct tcphdr *) (packet+SIZE_ETHERNET+size_ip); // pointer to the TCP header
-                tcp_handler(my_tcp, packet);
+                
+
+                size_TCP = (*(tcp_header+12) & 0xf0) >> 2;
+                ssl_header = tcp_header + size_TCP;
+
+                printf("############################ %d ##########################\n", size_TCP);
+                printf("1.##%d##\n", *(ssl_header+3));
+                printf("2.##%d##\n", *(ssl_header+4));
+                printf("1+2##%d##\n", ((*(ssl_header+3))+(*(ssl_header+4))));
+                short length = htons(*(ssl_header+3)) + (*(ssl_header+4));
+                printf("length = %d\n\n", length);
+
+                tcp_handler(my_tcp, packet, src_IP, dest_IP, ssl_header);    
             }
+            */
+
             break;
         default:
             break;
@@ -100,8 +121,10 @@ char* getHandlerOnline(char* interfaceName){
             if((handler = pcap_open_live(interfaceName, BUFSIZ, 1, 1000, errbuf)) == NULL){
                 return ERR_OPEN_LIVE;
             }else{
-                if (pcap_loop(handler,-1,mypcap_handler,NULL) == -1){
-                    return ERR_PCAP_LOOP;
+                const u_char *packet;
+                struct pcap_pkthdr header;
+                while ((packet = pcap_next(handler,&header)) != NULL){
+                    mypcap_handler(header, packet, ssl_list);
                 }
                 pcap_close(handler);
                 pcap_freealldevs(alldev);
@@ -121,8 +144,10 @@ char* getHandlerOffline(char* fileName){
     if((handler = pcap_open_offline(fileName, errbuf)) == NULL){
         return ERR_OPEN_FILE;
     }
-    if (pcap_loop(handler,-1,mypcap_handler,NULL) == -1){
-        return ERR_PCAP_LOOP;
+    const u_char *packet;
+    struct pcap_pkthdr header;
+    while ((packet = pcap_next(handler,&header)) != NULL){
+        mypcap_handler(header, packet, ssl_list);
     }
     pcap_close(handler);
     ssl_destructor(ssl_list);
--- sslParser.h
+++ sslParser.h
@@ -6,7 +6,6 @@
 #define ERR_WRONG_INPUT_DEVICE "Your network interface can`t be use."
 #define ERR_OPEN_LIVE "pcap_open_live() failed."
 #define ERR_OPEN_FILE "Can`t open file for reading."
-#define ERR_PCAP_LOOP "pcap_loop() failed"
 
 char* getHandlerOnline(char* interfaceName);
 char* getHandlerOffline(char* fileName);
