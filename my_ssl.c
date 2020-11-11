6537f9fa528f02dee76262742b8c83cc5b396397
Patrik Ondriga <xondri08@vutbr.cz>
2020-11-12 00:02:34 +0100

    Fix bugs with constructor and destructor

--- my_ssl.c
+++ my_ssl.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-ssl_con* ssl_constructor(int timestamp, char* client_IP, int client_PORT, char* server_IP, int server_PORT, char* sni, int duration_sec){
+ssl_con* ssl_constructor(char* timestamp, char* client_IP, int client_PORT, char* server_IP, int server_PORT, int duration_sec){
     ssl_con* ssl_con_p = (ssl_con*) malloc(sizeof(ssl_con));
     if(ssl_con_p == NULL){
         return NULL;
     }
+    ssl_con_p->timestamp = (char*) malloc(sizeof(char)*(strlen(timestamp)+1));
+    if(ssl_con_p->timestamp == NULL){
+        free(ssl_con_p);
+    }
     ssl_con_p->client_IP = (char*) malloc(sizeof(char)*(strlen(client_IP)+1));
     if(ssl_con_p->client_IP == NULL){
+        free(ssl_con_p->timestamp);
         free(ssl_con_p);
         return NULL;
     }
     ssl_con_p->server_IP = (char*) malloc(sizeof(char)*(strlen(server_IP)+1));
     if(ssl_con_p->server_IP == NULL){
+        free(ssl_con_p->timestamp);
         free(ssl_con_p->client_IP);
         free(ssl_con_p);
         return NULL;
     }
-    ssl_con_p->sni = (char*) malloc(sizeof(char)*(strlen(sni)+1));
-    if(ssl_con_p->sni == NULL){
-        free(ssl_con_p->client_IP);
-        free(ssl_con_p->server_IP);
-        free(ssl_con_p);
-        return NULL;
-    }
-    ssl_con_p->timestamp = timestamp;
-    ssl_con_p->client_IP = client_IP;
+    strcpy(ssl_con_p->timestamp, timestamp);
+    strcpy(ssl_con_p->client_IP, client_IP);
     ssl_con_p->client_PORT = client_PORT;
-    ssl_con_p->server_IP = server_IP;
+    strcpy(ssl_con_p->server_IP, server_IP);
     ssl_con_p->server_PORT = server_PORT;
-    ssl_con_p->sni = sni;
+    ssl_con_p->sni = NULL;
+    ssl_con_p->bytes = 0;
+    ssl_con_p->packets = 0;
     ssl_con_p->duration_sec = duration_sec;
     ssl_con_p->next = NULL;
     return ssl_con_p;
@@ -47,9 +48,12 @@ void ssl_addOnEnd(ssl_con* ssl_con_p, ssl_con* new_ssl){
 void ssl_destructor_all(ssl_con* ssl_con_p){
     ssl_con* tmp = NULL;
     while (ssl_con_p != NULL){
+        free(ssl_con_p->timestamp);
         free(ssl_con_p->client_IP);
         free(ssl_con_p->server_IP);
-        free(ssl_con_p->sni);
+        if(ssl_con_p->sni != NULL){
+            free(ssl_con_p->sni);
+        }
         tmp = ssl_con_p->next;
         free(ssl_con_p);
         ssl_con_p = tmp;
@@ -60,14 +64,29 @@ void ssl_destructor(ssl_con** ssl_con_p, ssl_con* destroyMe){
     ssl_con* tmp = *ssl_con_p;
     if(tmp == destroyMe){
         *ssl_con_p = tmp->next;
-        return;
+    }else{
+        while(tmp->next != destroyMe){
+            tmp = tmp->next;
+        }
+        tmp->next = destroyMe->next;
     }
-    while(tmp->next != destroyMe){
-        tmp = tmp->next;
-    }
-    tmp->next = destroyMe->next;
+    free(destroyMe->timestamp);
     free(destroyMe->client_IP);
     free(destroyMe->server_IP);
-    free(destroyMe->sni);
+    if(destroyMe->sni != NULL){
+        free(destroyMe->sni);
+    }
     free(destroyMe);
+}
+
+bool ssl_addSNI(ssl_con* ssl_con_p, char* sni){
+    if(ssl_con_p->sni != NULL){
+        free(ssl_con_p->sni);
+    }
+    ssl_con_p->sni = (char*) malloc(sizeof(char) * (strlen(sni)+1));
+    if(ssl_con_p->sni == NULL){
+        return false;
+    }
+    ssl_con_p->sni = sni;
+    return true;
 }
--- my_ssl.h
+++ my_ssl.h
@@ -1,10 +1,12 @@
 #ifndef HEADER_MY_SSL_H
 #define HEADER_MY_SSL_H
 
+#include <stdbool.h>
+
 #define SSL_LENGTH = 3;
 
 typedef struct ssl_connection{
-    int timestamp;
+    char* timestamp;
     char* client_IP;
     int client_PORT;
     char* server_IP;
@@ -16,9 +18,10 @@ typedef struct ssl_connection{
     struct ssl_connection* next;
 }ssl_con;
 
-ssl_con* ssl_constructor(int timestamp, char* client_IP, int client_PORT, char* server_IP, int server_PORT, char* sni, int duration_sec);
+ssl_con* ssl_constructor(char* timestamp, char* client_IP, int client_PORT, char* server_IP, int server_PORT, int duration_sec);
 void ssl_addOnEnd(ssl_con* ssl_con_p, ssl_con* new_ssl);
 void ssl_destructor_all(ssl_con* ssl_con_p);
 void ssl_destructor(ssl_con** ssl_con_p, ssl_con* destroyMe);
+bool ssl_addSNI(ssl_con* ssl_con_p, char* sni);
 
 #endif
